607f765870aa834a027b351c9b6ac7237c302266
Doris Bilic <dorisbilic11@gmail.com>
2025-09-15 16:16:34 +0200

    Create table book and author in db/migration

---
+++ src/main/resources/db/migration/V1.0__author_table.create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE author
+(
+    id          BIGINT(20) AUTO_INCREMENT NOT NULL,
+    first_name  VARCHAR(255)              NOT NULL,
+    last_name   VARCHAR(255)              NOT NULL,
+    PRIMARY KEY (id)
+);
