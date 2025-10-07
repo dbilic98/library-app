d6d7b77bc9f6b48195ac1229d8b0790a2c608bea
Doris Bilic <dorisbilic11@gmail.com>
2025-10-07 14:01:37 +0200

    Format code

--- src/main/java/com/libraryapp/domain/model/Author.java
+++ src/main/java/com/libraryapp/domain/model/Author.java
@@ -12,29 +12,30 @@ import java.util.Objects;
 @Setter
 public class Author {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    private String firstName;
+  private String firstName;
 
-    private String lastName;
+  private String lastName;
 
-    public Author() {}
+  public Author() {
+  }
 
-    public Author(Long id, String firstName, String lastName) {
-        Objects.requireNonNull(firstName,"firstName is mandatory" );
-        Objects.requireNonNull(lastName,"lastName is mandatory" );
+  public Author(Long id, String firstName, String lastName) {
+    Objects.requireNonNull(firstName, "firstName is mandatory");
+    Objects.requireNonNull(lastName, "lastName is mandatory");
 
-        if(firstName.length() < 3){
-            throw new IllegalArgumentException("firstName must be at least 3 characters");
-        }
-        if(lastName.length() < 3){
-            throw new IllegalArgumentException("lastName must be at least 3 characters");
-        }
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-
+    if (firstName.length() < 3) {
+      throw new IllegalArgumentException("firstName must be at least 3 characters");
     }
+    if (lastName.length() < 3) {
+      throw new IllegalArgumentException("lastName must be at least 3 characters");
+    }
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+
+  }
 }
--- src/test/java/com/libraryapp/domain/model/AuthorTest.java
+++ src/test/java/com/libraryapp/domain/model/AuthorTest.java
@@ -1,28 +1,28 @@
 package com.libraryapp.domain.model;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 class AuthorTest {
 
-    @Test
-    void whenFirstNameIsNull_ThrowsException() {
-        assertThrows(NullPointerException.class, () -> new Author(1L, null, "Bilic"));
-    }
+  @Test
+  void whenFirstNameIsNull_ThrowsException() {
+    assertThrows(NullPointerException.class, () -> new Author(1L, null, "Bilic"));
+  }
 
-    @Test
-    void whenLastNameIsNull_ThrowsException() {
-        assertThrows(NullPointerException.class, () -> new Author(1L, "Doris", null));
-    }
+  @Test
+  void whenLastNameIsNull_ThrowsException() {
+    assertThrows(NullPointerException.class, () -> new Author(1L, "Doris", null));
+  }
 
-    @Test
-    void whenFirstNameLengthIsLessThan3_ThrowsException() {
-        assertThrows(IllegalArgumentException.class, () -> new Author(1L, "Do", "Bilic"));
-    }
+  @Test
+  void whenFirstNameLengthIsLessThan3_ThrowsException() {
+    assertThrows(IllegalArgumentException.class, () -> new Author(1L, "Do", "Bilic"));
+  }
 
-    @Test
-    void whenLastNameLengthIsLessThan3_ThrowsException() {
-        assertThrows(IllegalArgumentException.class, () -> new Author(1L, "Doris", "Bi"));
-    }
+  @Test
+  void whenLastNameLengthIsLessThan3_ThrowsException() {
+    assertThrows(IllegalArgumentException.class, () -> new Author(1L, "Doris", "Bi"));
+  }
 }
