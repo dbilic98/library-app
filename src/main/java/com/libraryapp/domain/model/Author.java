c8328e8f37fe59f0139f03babd5bd08571d21ebc
Doris Bilic <dorisbilic11@gmail.com>
2025-10-09 11:53:51 +0200

    Refactoring imports

--- src/main/java/com/libraryapp/domain/model/Author.java
+++ src/main/java/com/libraryapp/domain/model/Author.java
@@ -1,10 +1,13 @@
 package com.libraryapp.domain.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Objects;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.Objects;
 
 @Entity
 @Table(name = "author")
