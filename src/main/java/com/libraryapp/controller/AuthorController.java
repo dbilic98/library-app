9c83217a55a242125273225769d41a4f1a215ea7
Doris Bilic <dorisbilic11@gmail.com>
2026-01-28 14:29:56 +0100

    Error correction

--- src/main/java/com/libraryapp/controller/AuthorController.java
+++ src/main/java/com/libraryapp/controller/AuthorController.java
@@ -10,9 +10,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/author")
+@RequestMapping("/authors")
 public class AuthorController {
 
   private final AuthorService authorService;
@@ -29,8 +29,15 @@ public class AuthorController {
     this.authorService = authorService;
   }
 
-  @GetMapping("/{id}")
-  public ResponseAuthorDto findAuthorById(@PathVariable("id") Long id) {
+  private ResponseAuthorDto toResponseDto(Author author) {
+    return new ResponseAuthorDto(
+        author.getId(),
+        author.getFirstName(),
+        author.getLastName());
+  }
+
+  @GetMapping("/{authorsId}")
+  public ResponseAuthorDto getAuthorById(@PathVariable("authorsId") Long id) {
     Author author = authorService.findAuthorById(id);
     return new ResponseAuthorDto(
         author.getId(),
@@ -39,19 +46,12 @@ public class AuthorController {
   }
 
   @GetMapping
-  public PaginatedResponse<ResponseAuthorDto> findAllAuthors(
+  public PaginatedResponse<ResponseAuthorDto> getPaginatedAuthors(
       @RequestParam(defaultValue = "0") int pageNumber,
       @RequestParam(defaultValue = "10") int pageSize) {
-    Page<Author> allAuthors = authorService.findAllAuthors(pageNumber, pageSize);
-    Page<ResponseAuthorDto> map = allAuthors.map(this::toResponseDto);
-    return new PaginatedResponse<>(map);
-  }
-
-  private ResponseAuthorDto toResponseDto(Author author) {
-    return new ResponseAuthorDto(
-        author.getId(),
-        author.getFirstName(),
-        author.getLastName());
+    Page<Author> authorsPage = authorService.findAllAuthors(pageNumber, pageSize);
+    Page<ResponseAuthorDto> authorsDtoPage = authorsPage.map(this::toResponseDto);
+    return new PaginatedResponse<>(authorsDtoPage);
   }
 
   @PostMapping
@@ -64,9 +64,9 @@ public class AuthorController {
         createdAuthor.getLastName());
   }
 
-  @PutMapping("/{id}")
+  @PatchMapping("/{authorsId}")
   @ResponseStatus(HttpStatus.OK)
-  public ResponseAuthorDto updateAuthor(@Valid @PathVariable("id") Long id,
+  public ResponseAuthorDto patchAuthor(@Valid @PathVariable("authorsId") Long id,
       @RequestBody RequestAuthorDto requestAuthorDto) {
     Author updatedAuthor = authorService.updateAuthor(id, requestAuthorDto);
     return new ResponseAuthorDto(
@@ -75,9 +75,9 @@ public class AuthorController {
         updatedAuthor.getLastName());
   }
 
-  @DeleteMapping("/{id}")
+  @DeleteMapping("/{authorsId}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void deleteAuthor(@PathVariable("id") Long id) {
+  public void deleteAuthor(@PathVariable("authorsId") Long id) {
     authorService.deleteAuthor(id);
   }
 }
