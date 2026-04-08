c301489999e3dcfdb23859e4bc559e80e1a29957
Doris Bilic <dorisbilic11@gmail.com>
2026-04-08 17:53:25 +0200

    Transferred logic from the controller to the service

--- src/main/java/com/libraryapp/controller/UserController.java
+++ src/main/java/com/libraryapp/controller/UserController.java
@@ -3,9 +3,8 @@ package com.libraryapp.controller;
 import com.libraryapp.controller.request.RequestUserDto;
 import com.libraryapp.controller.response.ResponseUserDto;
 import com.libraryapp.domain.model.User;
-import com.libraryapp.domain.repository.UserRepository;
+import com.libraryapp.domain.service.UserService;
 import jakarta.validation.Valid;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,20 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
 
-  private final UserRepository userRepository;
-  private final PasswordEncoder passwordEncoder;
+  private final UserService userService;
 
-  public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-    this.userRepository = userRepository;
-    this.passwordEncoder = passwordEncoder;
+  public UserController(UserService userService) {
+    this.userService = userService;
   }
 
   @PostMapping
   public ResponseUserDto createUser(@Valid @RequestBody RequestUserDto requestUserDto) {
-    User userToSave = new User();
-    userToSave.setUsername(requestUserDto.username());
-    userToSave.setPassword(passwordEncoder.encode(requestUserDto.password()));
-    User createdUser = userRepository.save(userToSave);
-    return new ResponseUserDto(createdUser.getUsername());
+    User createdUser = userService.createUser(requestUserDto);
+    return new ResponseUserDto(
+        createdUser.getUsername());
   }
 }
