0c62cb3b5cf28127f64cc4837bf76e40fdc81c49
Doris Bilic <dorisbilic11@gmail.com>
2026-04-08 17:56:52 +0200

    Add regex pattern

--- src/main/java/com/libraryapp/controller/request/RequestUserDto.java
+++ src/main/java/com/libraryapp/controller/request/RequestUserDto.java
@@ -1,16 +1,20 @@
 package com.libraryapp.controller.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record RequestUserDto(
 
-    @NotBlank(message = "User is mandatory")
+    @NotBlank(message = "Username is mandatory")
     @Size(min = 3, message = "Username must have at least 3 character")
     String username,
 
     @NotBlank(message = "Password is mandatory")
-    @Size(min = 3, message = "Password must have at least 8 character")
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).{8,}$",
+        message = "Password must be at least 8 characters, include one uppercase, one lowercase, and one special character"
+    )
     String password
 ) {
 
